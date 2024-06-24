02accfa0ca78c813c9a5231bd3bb76dd57be9a71
TooniOlaniyan <asalamlatif@gmail.com>
2024-06-24 21:51:59 +0100

    header navigation setup

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full">
+      <div className="container  px-4 py-2 flex items-center justify-between">
+        <div className="flex-shrink-0">
+          <Link href="/">
+            <Image
+              src="./logo.svg"
+              width={150}
+              height={150}
+              alt="logo"
+              className="object-contain"
+            />
+          </Link>
+        </div>
 
-export default Header
+        <nav className="flex gap-20">
+          <Link href="/about-us">About Us</Link>
+          <Link href="/services">Services</Link>
+          <Link href="/works">Works</Link>
+        </nav>
+
+        <div className="flex-shrink-0">
+          <Link href="/contact-us">Contact us</Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
